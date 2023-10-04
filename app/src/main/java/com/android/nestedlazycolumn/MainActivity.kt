6d20264c6c3c11e0846f09e5871fcbe858b6f136
“Aashis” <thapaaashis20@gmail.com>
2023-10-04 16:55:46 +0545

    Update selected item delay in it

--- app/src/main/java/com/android/nestedlazycolumn/MainActivity.kt
+++ app/src/main/java/com/android/nestedlazycolumn/MainActivity.kt
@@ -1,24 +1,37 @@
 package com.android.nestedlazycolumn
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.android.nestedlazycolumn.ui.theme.NestedLazyColumnTheme
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,6 +45,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(DelicateCoroutinesApi::class)
 @Composable
 fun NestedColumn() {
 
@@ -65,15 +79,39 @@ fun NestedColumn() {
 
     LazyColumn {
 
-        item { /* Content for the header */ }
+        item { Text(text = "This is Header context") }
 
         items(categories.size) { index ->
             Text(text = categories[index].title)
 
-            categories[index].products.forEach {
-                Text(text = it.name + ".........." + it.price)
-            }
+            var selectedItem by remember { mutableStateOf<String?>(null) }
+            var canClick by remember { mutableStateOf(true) }
 
+            categories[index].products.forEach {
+                Card(
+                    modifier = Modifier.clickable {
+                        if (canClick) {
+                            canClick = false
+                            selectedItem = it.name
+                            Log.d("click", it.name)
+                            // Delay for 10 seconds
+                            GlobalScope.launch {
+                                delay(2000)
+                                selectedItem = null
+                                canClick = true
+                            }
+                        }
+                    }
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .padding(50.dp)
+                    ) {
+                        Text(text = it.name + ".........." + it.price, fontSize = 17.sp)
+                    }
+                }
+
+            }
             /*repeat(10) { index ->
                 // Code to be executed for each iteration
                 Text(text = "This is Inner Nested")
